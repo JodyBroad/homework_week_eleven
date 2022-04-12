fe16c926c57867cff8b4966e7d4d2c3ee7c6aae3
Jody Broad <msjodybroad@gmail.com>
2022-04-12 16:14:21 +0100

    services wrangling

--- Nail_bar/application/services.py
+++ Nail_bar/application/services.py
@@ -1,17 +1,14 @@
 # in reality, we would be calling this info from a database, dictionary of services to use for price_list
 
-# move the services dictionary here
-
 # to then make a dynamic list we could use for a drop down on a form:
 # look through the dictionary using enumerate, this gives the index number for each entry which we can use as the
 # back-end descriptor, get it to return the name of each service e.g. French Manicure etc
 # save this as a separate list (in tuples, so (0, "French Manicure) etc etc
 # and then use this to populate the drop down using SelectField
 
-# also clean stuff up - have a seperate routes file, and an app file, we don't have __init__.py file as isn't currently
-# a package and we also don't have app.py, we have nail_bar_app
 
 services = [
+
     {
         'service': 'French Manicure',
         'price': '£30',
@@ -45,5 +42,42 @@ services = [
 
 ]
 
+# this is basically the list i need to generate to get a dynamic list - but how do i get it to make this for me?
 services_list = [(0, "French Manicure"), (1, "Gel polish"), (2, "Soak and remove existing gel polish"),
                  (3, "Acrylic nails - full set"), (4, "Acrylic nails - infills")]
+
+# this gives the service list names as a basic list, need this to be a list of tuples with index number
+# def services_dropdown():
+#     service_list = []
+#     i = 0
+#     for service in services:
+#         for service_name in services:
+#             service_list.append(services[i]['service'])
+#             i += 1
+#         return service_list
+#
+# print(services_dropdown())
+
+# how do i get it in the tuple format victoria suggested with enumerate?
+# def services_dropdown():
+#     service_list = []
+#     for service in services:
+#         for count, service_name in enumerate(services):
+#             service_list.append(services[count]['service'])
+#         return (count, service_list)
+#
+# print(services_dropdown())
+
+# got it doing the first tuple, but this is still very much not right, why isn't it looping over the other dictionaries?
+def services_dropdown():
+    service_list = []
+    list_entry = ()
+    for index in range(len(services)):
+        for count, key in enumerate(services[index]):
+            list_entry = (count, services[index]['service'])
+            service_list.append(list_entry)
+            return list_entry
+    service_list.append(list_entry)
+    return service_list
+
+print(services_dropdown())
